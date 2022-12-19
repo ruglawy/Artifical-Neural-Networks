50ea209254a003ddb3136e53d7a023b0da73e7b1
ruglawy <107264996+ruglawy@users.noreply.github.com>
2022-12-19 04:21:29 +0200

    Made Main Screen that has a button that takes an image.

---
+++ Testingyabro/HomeScreen.cpp
@@ -0,0 +1,2 @@
+#include "HomeScreen.h"
+
--- Testingyabro/Source.cpp
+++ Testingyabro/Source.cpp
@@ -2,13 +2,15 @@
 #include "NeuralScreen.h"
 #include "MyForm5.h"
 #include "TestwithoutPanel.h"
+#include "HomeScreen.h"
 
 using namespace System;
 using namespace System::Windows::Forms;
+[STAThreadAttribute]
 void main(array<String^>^ args)
 {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
-	Testingyabro::NeuralScreen form;
+	Testingyabro::HomeScreen form;
 	Application::Run(% form);
 }
